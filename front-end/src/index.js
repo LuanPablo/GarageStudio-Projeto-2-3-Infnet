50f9aaa9abe10683e13ff827b8861bbeb4a57f70
Luan Lima <luan.pablo15@hotmail.com>
2022-07-13 23:19:24 -0300

    Atualizando projeto

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/css/styles.css';
+import './Assets/css/styles.css';
 import { BrowserRouter } from 'react-router-dom';
 
 
