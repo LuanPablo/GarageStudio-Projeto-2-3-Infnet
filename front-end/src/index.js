849317ce9e340dc79b023c7d90fee88d4a5712ae
Luan Lima <luan.pablo15@hotmail.com>
2022-07-22 00:56:17 -0300

    atualizando branch

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Assets/css/styles.css';
+import 'react-toastify/dist/ReactToastify.css'
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './Assets/global'
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
     <BrowserRouter>
       <App />
+    <ToastContainer />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
