d8b6d9f63247c74d3af297b6e332b5d51ccfe2a0
Luan Lima <luan.pablo15@hotmail.com>
2022-07-22 03:31:55 -0300

    correções

--- front-end/src/componentes/Layout/Header.js
+++ front-end/src/componentes/Layout/Header.js
@@ -1,21 +1,29 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Logo from '../../Assets/img/logo.png'
+import Logo from '../../assets/img/logo-infnet.png'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+import { selectIsUserLoggedIn } from "../../store/User/User.selectors";
 
-export function Header() {
+export function Header () {
+  const isUserLoggedIn = useSelector(selectIsUserLoggedIn)
   return (
     <header>
-      <Navbar className='main-navbar' expand='md'>
-        <Container>
-          <Navbar.Brand as={Link} to='/'>
-            <img src={Logo} alt='Garage Studio' width={115} height={19} />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar-header' />
-          <Navbar.Collapse id='navbar-header'>
-            <Nav className='ms-auto'>
-              <Nav.Link as={Link} to='/'>Home</Nav.Link>
-              <Nav.Link as={Link} to='/studios'>Agendar</Nav.Link>
-              <Nav.Link as={Link} to='/'>Login</Nav.Link>
+    <Navbar className='main-navbar' expand='md'>
+      <Container>
+        <Navbar.Brand as={Link} to='/'>
+          <img src={Logo} alt='Garage Studio' width={115} height={19} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbar-header' />
+        <Navbar.Collapse id='navbar-header'>
+          <Nav className='ms-auto'>
+            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+            <Nav.Link as={Link} to='/studios'>Agendar</Nav.Link>
+              {isUserLoggedIn ? (
+                <NavLinkStyled forwardedAs={Link} to='/portal'>Entrar</NavLinkStyled>
+              ) : (
+                <NavLinkStyled forwardedAs={Link} to='/portal/login'>Login</NavLinkStyled>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
